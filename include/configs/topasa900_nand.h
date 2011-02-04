ae48ba9188b4d3bfdd515c602a3d843d7b9f4f64
Florian Boor <florian@kernelconcepts.de>
2011-02-04 22:57:02 +0100

    Fix default root mtd device for topasa900.

--- include/configs/topasa900_nand.h
+++ include/configs/topasa900_nand.h
@@ -118,7 +118,7 @@
 
 #define CONFIG_EXTRA_ENV_SETTINGS	"update_kernel=dhcp uImage-topasa900; nand erase kernel; nand write ${fileaddr} kernel\0" \
 					"update_rootfs=dhcp rootfs-topasa900; nand erase rootfs; nand write ${fileaddr} rootfs ${filesize}\0" \
-                                    "rootfs_jffs2=root=/dev/mtdblock4 rootfstype=jffs2\0" \
+                                    "rootfs_jffs2=root=/dev/mtdblock5 rootfstype=jffs2\0" \
                                     "rootfs_ubifs=ubi.mtd=4 root=ubi0:rootfs rootfstype=ubifs\0" \
                                    	"rootfs_base=setenv rootfs ${rootfs_jffs2}\0"\
                                     "bootargs_base=setenv bootargs console=ttyS0,115200n8 ${rootfs} ${mtdparts}\0" \
